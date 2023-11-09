ea80d8dcefa26556e80cc80c5298af81d6a22fed
linus <l1nu5.h4id1@gmail.com>
2023-11-09 22:34:33 +0100

    bar chart

--- Bar_Chart.py
+++ Bar_Chart.py
@@ -17,15 +17,21 @@ solution_approx = main.transform_back(rho_approx_transformed, lamb, delta_time)
 rho_exact_transformed = main.solve_exact(rho_0, mu, time_steps, delta_time)
 solution_exact = main.transform_back(rho_exact_transformed, lamb, delta_time)
 
-width = 0.4
-x_cords = numpy.arange(dimensions) + 1
-for time_step in range(0, time_steps+1, step_skip):
-    fig, ax = pyplot.subplots()
-    diag_approx = numpy.diag(solution_approx[time_step])
-    diag_exact = numpy.diag(solution_exact[time_step])
-    ax.bar(x_cords - width, diag_approx, width, align='edge')
-    ax.bar(x_cords, diag_exact, width, align='edge')
-    ax.set_title('solution for t = {:.1f}'.format(time_step*delta_time))
-    ax.legend(("approximate","exact"))
-    pyplot.show()
-    pyplot.close(fig)
+
+def plot_bar(approx, exact, title: str):
+    width = 0.4
+    x_cords = numpy.arange(dimensions) + 1
+    for time_step in range(0, time_steps + 1, step_skip):
+        fig, ax = pyplot.subplots()
+        diag_approx = numpy.diag(approx[time_step])
+        diag_exact = numpy.diag(exact[time_step])
+        ax.bar(x_cords - width, diag_approx, width, align='edge')
+        ax.bar(x_cords, diag_exact, width, align='edge')
+        ax.set_title('{:} for t = {:.1f}'.format(title, time_step * delta_time))
+        ax.legend(("approximate", "exact"))
+        pyplot.show()
+        pyplot.close(fig)
+
+
+plot_bar(rho_approx_transformed, rho_exact_transformed, "transformed solution rho tilde")
+plot_bar(solution_approx, solution_exact, "solution rho")
